15e80062d5b217df39272d096174f00f5531fa37
LV <1756249030@qq.com>
2025-12-20 18:03:03 +0800

    junit

--- src/junit/junit.java
+++ src/junit/junit.java
@@ -1,4 +1,19 @@
 package junit;
 
+import org.junit.jupiter.api.Test;
+
 public class junit {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void m1(){
+        System.out.println("m1");
+    }
+
+    @Test
+    public void m2(){
+        System.out.println("m2");
+    }
 }

37340056e033283555aeb0650116e8c209944342
lvshijie <lovw1016@126.com>
2026-01-04 17:29:03 +0800

    test04 : init

---
+++ src/com/thread/Test04.java
@@ -0,0 +1,11 @@
+package com.thread;
+
+public class Test04 {
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+
+        // 优先级 PRIORITY  1 5 10
+//        thread.getPriority();
+        // interrupt 中断线程 但是没有真正结束线程  一般用于中断正在休眠的线程
+    }
+}

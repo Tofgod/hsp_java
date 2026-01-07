cc6e0d4ca8c431ffd371c31f2d20b54ca0150b88
lvshijie <lovw1016@126.com>
2026-01-07 17:52:03 +0800

    fix

--- src/com/thread/Test07.java
+++ src/com/thread/Test07.java
@@ -1,15 +1,17 @@
 package com.thread;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.concurrent.locks.LockSupport;
 
 public class Test07 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Thread 线程的状态
         System.out.println(Thread.State.NEW);//尚未启动
 
         System.out.println(Thread.State.RUNNABLE);
         //正在运行 可运行状态 分为两个状态。一个是就绪状态 ready  一个是运行状态running
         // 当线程进入RUNNABLE状态 不是立即就会执行 是由系统调度决定的
+        // 线程挂起或者让步  会从running 到 ready  是由系统调度决定的
 
 
         //* o 表示本线程 t表示其他线程
@@ -31,8 +33,35 @@ public class Test07 {
         System.out.println(Thread.State.TERMINATED);//已经退出
 
 
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                while (true) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    a++;
+                    if (a > 10) {
+                        break;
+                    }
+                }
+
+            }
+        });
+
+        thread.start();
+
+        while (Thread.State.TERMINATED != thread.getState()){
+            System.out.println(thread.getState());
+            Thread.sleep(1000);
+        }
 
 
 
     }
 }
+
+
---
+++ src/com/thread/synchronized_.java
@@ -0,0 +1,10 @@
+package com.thread;
+
+public class synchronized_ {
+    public static void main(String[] args) {
+
+        // 线程同步机制  保证一些数据 在任何同一时刻最多有一个线程访问 保证数据的完整性
+        // 也可以这样理解 当一个线程对一个内存进行操作的时候  其他线程不可以对该内存地址进行操作 直到该线程操作完毕
+
+    }
+}

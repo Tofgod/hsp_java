08a51a4474bf9ccda305f3eb107e70a64f015eb5
lvshijie <lovw1016@126.com>
2026-03-18 18:06:24 +0800

    class

--- src/fanshe/Class01.java
+++ src/fanshe/Class01.java
@@ -3,7 +3,14 @@ package fanshe;
 public class Class01 {
     public static void main(String[] args) {
         // Class 特点梳理
-        // Class 不是牛new出来的 是系统创建的  通过 类加载器 ClassLoader类的loaderClass 创建出来的
+        // Class 不是牛new出来的 是系统创建的  通过 类加载器 ClassLoader类的loaderClass 创建出来的  类的价值只会有一次
         //
+
+        Cat cat = new Cat();
+        try {
+            Class<?> aClass = Class.forName("fanshe.Cat");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
